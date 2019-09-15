2ead8526d3aa1d896f5fee2799df9e6113ee1d19
Alfonso Pruneda-Suarez <alfonso.pru@outlook.com>
2019-09-15 04:40:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,20 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf_names)
+  dwarf_names.each_with_index do |name, idx|
+    puts "#{idx+1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect!{|element| element.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.each do |word|
+    if word.length > 4
+      return true
+    end
+  end
+  false
 end
 
 def find_the_cheese# code an argument here
